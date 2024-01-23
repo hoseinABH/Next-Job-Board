260f520c79b968b80c9feb5e7e2b50e435bee893
hosein <hosein.abolhasani1998@gmail.com>
2024-01-23 11:26:57 +0330

    fix:build error

--- src/store/Resume/resume.sagas.ts
+++ src/store/Resume/resume.sagas.ts
@@ -4,28 +4,8 @@
  */
 
 // Utilities
-import { all, delay, put, takeLatest } from 'redux-saga/effects';
-// Actions
-import CommonActions from './resume.actions';
-// Constants
-import * as types from './resume.constants';
-
-function* someSagasAction() {
-  try {
-    let obj: any = {};
-    yield put(CommonActions.setAppLoading(true));
-    yield delay(4000);
-    if (!obj.name) {
-      throw new Error('Something went wrong!');
-    }
-    console.log('some action');
-  } catch (error) {
-    console.log(error);
-  } finally {
-    yield put(CommonActions.setAppLoading(false));
-  }
-}
+import { all } from 'redux-saga/effects';
 
 export default function* networkListeners() {
-  yield all([takeLatest(types.SAGAS_SOME_ACTION, someSagasAction)]);
+  yield all([]);
 }
