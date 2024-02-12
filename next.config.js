e39306d785203075f6fc9e7f25095f57912863a0
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-02-12 10:11:22 +0330

    feat:add dependency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
