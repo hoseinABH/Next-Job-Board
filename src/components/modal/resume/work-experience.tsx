90e9590e61524e77614bcd101abf98c8805f93d6
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-02-19 12:11:39 +0330

    feat:add work experience form validation

--- src/components/modal/resume/work-experience.tsx
+++ src/components/modal/resume/work-experience.tsx
@@ -1,9 +1,7 @@
 // Common components
+import ControlledInput from '@/components/controlled-input';
+import ControlledCheckbox from '@/components/controlled-checkbox';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import { Checkbox } from '@/components/ui/checkbox';
 import {
   Dialog,
   DialogContent,
@@ -12,59 +10,111 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { Form } from '@/components/ui/form';
+// Utilities
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 // Actions
 import ResumeActions from '@/store/Resume/resume.actions';
 // Hooks
 import { useAppSelector, useAppDispatch } from '@/hooks/store';
+import { useForm } from 'react-hook-form';
+
+const workExperienceFormSchema = z.object({
+  position: z.string().min(1, { message: 'عنوان شغلی را وارد کنید' }),
+  companyName: z.string().min(1, { message: 'نام شرکت را وارد کنید' }),
+  startDate: z.string().min(1, { message: 'تاریخ شروع را وارد کنید' }),
+  endDate: z.string().min(1, { message: 'تاریخ پایان را وارد کنید' }),
+  description: z.string().min(1, { message: 'توضیحات را وارد کنید' }),
+  isCurrent: z.boolean(),
+});
+
+type FormData = typeof workExperienceFormSchema;
 
 export function WorkExperienceModal() {
   const dispatch = useAppDispatch();
-  const { modals } = useAppSelector((state) => state.resume);
+  const { modals, loading } = useAppSelector((state) => state.resume);
+  const form = useForm<z.infer<FormData>>({
+    resolver: zodResolver(workExperienceFormSchema),
+    defaultValues: {
+      position: '',
+      companyName: '',
+      startDate: '',
+      endDate: '',
+      description: '',
+      isCurrent: false,
+    },
+  });
+
+  function onSubmit(values: z.infer<FormData>) {
+    dispatch(ResumeActions.createExperience(values));
+    form.reset();
+  }
   function onOpenChange(open: boolean) {
     dispatch(ResumeActions.setModalOpen(open, 'workExperience'));
+    form.reset();
   }
   return (
     <Dialog open={modals.workExperience} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-xl max-h-screen py-12 sm:py-4 overflow-auto">
+      <DialogContent className="max-w-xl max-h-screen pb-4 pt-12 sm:pt-4 overflow-auto">
         <DialogHeader>
           <DialogTitle>سوابق شغلی</DialogTitle>
           <DialogDescription>
             لطفا فیلد های مورد نظر را تکمیل نمایید
           </DialogDescription>
         </DialogHeader>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="space-y-2">
-            <Label htmlFor="title">عنوان شغلی</Label>
-            <Input id="title" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="companyName">نام شرکت</Label>
-            <Input id="companyName" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="companyLocation">محل شرکت</Label>
-            <Input id="companyLocation" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="startDate">تاریخ شروع</Label>
-            <Input id="startDate" dir="ltr" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="endDate">تاریخ پایان</Label>
-            <Input id="endDate" dir="ltr" />
-          </div>
-          <div className="flex items-center gap-x-2 self-end">
-            <Checkbox id="present" />
-            <Label htmlFor="present">در این شرکت مشغول به کار هستم</Label>
-          </div>
-          <div className="space-y-2 sm:col-span-2">
-            <Label htmlFor="description">توضیحات</Label>
-            <Textarea rows={6} id="description" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">ذخیره تغییرات</Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form
+            id="workExperience"
+            className="grid grid-cols-1 sm:grid-cols-2 gap-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <ControlledInput
+              control={form.control}
+              name="position"
+              label="عنوان شغلی"
+            />
+            <ControlledInput
+              control={form.control}
+              name="companyName"
+              label="نام شرکت"
+            />
+            <ControlledInput
+              control={form.control}
+              name="startDate"
+              label="تاریخ شروع"
+              inputProps={{ dir: 'ltr', type: 'date' }}
+            />
+            <ControlledInput
+              control={form.control}
+              name="endDate"
+              label="تاریخ پایان"
+              inputProps={{ dir: 'ltr', type: 'date' }}
+            />
+            <ControlledCheckbox
+              control={form.control}
+              name="isCurrent"
+              label="در این شرکت مشغول به کار هستم"
+            />
+            <ControlledInput
+              className="sm:col-span-2"
+              control={form.control}
+              name="description"
+              label="توضیحات"
+              isTextArea
+              inputProps={{ rows: 5 }}
+            />
+          </form>
+          <DialogFooter>
+            <Button
+              form="workExperience"
+              type="submit"
+              loading={loading.createExperience}
+            >
+              ذخیره تغییرات
+            </Button>
+          </DialogFooter>
+        </Form>
       </DialogContent>
     </Dialog>
   );
