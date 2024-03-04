6bdfaacab3f4d6c1990b3b6b1412387d83f665b8
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-03-04 10:49:36 +0330

    improve company dashboard layout

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -6,10 +6,10 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="relative flex min-h-screen">
+    <main className="relative flex min-h-screen overflow-hidden">
       <SideBar />
-      <main className="flex-1 h-screen mr-[300px]">
-        <div className="relative h-full">{children}</div>
+      <main className="flex-1 h-screen overflow-auto mr-[300px]">
+        <div className="relative">{children}</div>
       </main>
     </main>
   );
