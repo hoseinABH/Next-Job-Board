cb11f5242868039638dd0597c4f2249d57a8b36d
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-03-04 16:21:26 +0330

    change auth background color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
       <div className="container px-4 sm:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 h-full w-full border bg-background dark:bg-secondary/30 shadow-md rounded-lg min-h-full md:min-h-[700px] overflow-hidden">
           {children}
-          <div className="bg-zinc-950 dark:bg-secondary/40 hidden lg:flex items-center justify-center">
+          <div className="bg-secondary dark:bg-secondary/40 hidden lg:flex items-center justify-center">
             <Image
               src="/hero.webp"
               alt="صفحه ورود"
