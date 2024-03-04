c55e41ea5f40296cb2057fe3da0fdca13ea87dbc
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-03-04 14:34:55 +0330

    fix:logo styles

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -17,7 +17,7 @@ interface Props {
 export default function Logo({ className, onClick }: Props) {
   const { theme } = useTheme();
   const imgSrc = useMemo(
-    () => (theme === 'dark' ? '/logo-white.webp' : '/logo.webp'),
+    () => (theme === 'light' ? '/logo.webp' : '/logo-white.webp'),
     [theme]
   );
   return (
