ffb17b5b238b720fca94ffb09152e5925d44ab46
Hosein <hosein.abolhasani1998@gmail.com>
2024-03-10 20:54:51 +0330

    feat:implement protect and private routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+// Configs
+import * as Routes from '@/config/routes';
 
-export default async function middleware(request: NextRequest) {
-  console.log(request.cookies.get('session')?.value);
+const privateRoutes = [Routes.CV_MAKER];
+const protectedRoutes = [Routes.LOGIN, Routes.REGISTER];
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.get('session')?.value;
+  if (!token && privateRoutes.includes(req.nextUrl.pathname)) {
+    const absoluteURL = new URL(Routes.LOGIN, req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+  if (token && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const absoluteURL = new URL(Routes.HOME, req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
 }
