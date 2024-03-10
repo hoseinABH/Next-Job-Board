192d714b1e358cb4c5c91f2ebdfff2f368be9cab
Hosein <hosein.abolhasani1998@gmail.com>
2024-03-10 20:57:09 +0330

    chore:add matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,3 +16,15 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.redirect(absoluteURL.toString());
   }
 }
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
