2227f463b23037b77a2db1eca7b68e7fa42f49b1
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-03-11 08:41:20 +0330

    change auth routes array name in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import * as Routes from '@/config/routes';
 
 const privateRoutes = [Routes.CV_MAKER];
-const protectedRoutes = [Routes.LOGIN, Routes.REGISTER];
+const authenticationRoutes = [Routes.LOGIN, Routes.REGISTER];
 
 export default async function middleware(req: NextRequest) {
   const token = req.cookies.get('session')?.value;
@@ -11,7 +11,7 @@ export default async function middleware(req: NextRequest) {
     const absoluteURL = new URL(Routes.LOGIN, req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
-  if (token && protectedRoutes.includes(req.nextUrl.pathname)) {
+  if (token && authenticationRoutes.includes(req.nextUrl.pathname)) {
     const absoluteURL = new URL(Routes.HOME, req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
