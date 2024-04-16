276443d5dd5e5f09db381f99786bb7fe306755ed
Hosein <hosein.abolhasani1998@gmail.com>
2024-04-16 07:26:51 +0330

    improve job description style

--- src/app/(main)/jobs/[id]/_components/position-content.tsx
+++ src/app/(main)/jobs/[id]/_components/position-content.tsx
@@ -14,9 +14,9 @@ export default function PositionContent({ className, job }: Props) {
   return (
     <Card className={cn('', className)}>
       <CardContent className="p-6">
-        <h1>درباره موقعیت شغلی: </h1>
+        <h1 className="text-xl font-semibold">درباره موقعیت شغلی: </h1>
         <br />
-        <p>{job.description}</p>
+        <p className="leading-10">{job.description}</p>
       </CardContent>
     </Card>
   );
