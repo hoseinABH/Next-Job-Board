0203e9c982966e898476dd6c1209aeb1f48bba72
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2024-05-19 16:54:59 +0330

    fix:deployment error

--- src/app/(panel)/dashboard/positions/data.ts
+++ src/app/(panel)/dashboard/positions/data.ts
@@ -1,10 +1,10 @@
-import type { EducationDegree } from '@/types/user';
+import type { Grade } from '@/types/user';
 
 export interface Position {
   title: string;
   contract: string;
   salary: string;
-  educationDegree: EducationDegree;
+  educationDegree: Grade;
   isUrgent: boolean;
 }
 
@@ -13,49 +13,49 @@ export const positions: Position[] = [
     title: 'برنامه نویس',
     contract: 'قرارداد',
     salary: '250.00',
-    educationDegree: 'Bachelor',
+    educationDegree: 2,
     isUrgent: false,
   },
   {
     title: 'منابع انسانی',
     contract: 'قرارداد',
     salary: '150.00',
-    educationDegree: 'Bachelor',
+    educationDegree: 2,
     isUrgent: true,
   },
   {
     title: 'تحلیلگر',
     contract: 'مشاوره',
     salary: '350.00',
-    educationDegree: 'Master',
+    educationDegree: 3,
     isUrgent: false,
   },
   {
     title: 'برنامه نویس',
     contract: 'طولانی مدت',
     salary: '450.00',
-    educationDegree: 'MiddleSchoolDiploma',
+    educationDegree: 0,
     isUrgent: true,
   },
   {
     title: 'تست نویس',
     contract: 'قرارداد',
     salary: '550.00',
-    educationDegree: 'Master',
+    educationDegree: 3,
     isUrgent: false,
   },
   {
     title: 'بازاریابی',
     contract: 'پاره وقت',
     salary: '200.00',
-    educationDegree: 'Bachelor',
+    educationDegree: 2,
     isUrgent: false,
   },
   {
     title: 'مدیریت',
     contract: 'استخدام رسمی',
     salary: '300.00',
-    educationDegree: 'Master',
+    educationDegree: 3,
     isUrgent: false,
   },
 ];
