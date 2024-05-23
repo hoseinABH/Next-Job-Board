617f0823d441cc8ecaff3cdb0b19d35eafa7b4c5
Hosein <hosein.abolhasani1998@gmail.com>
2024-05-23 16:29:12 +0330

    fix:unused file

--- src/types/panel.ts
+++
@@ -1,9 +0,0 @@
-import type { DialogData } from './common';
-
-export type ModalKeys = 'createPosition' | 'resumePreview';
-export type PanelModals = Record<ModalKeys, boolean>;
-
-export type LoadingKeys = 'getAllPositions';
-export type PanelLoading = Record<LoadingKeys, boolean>;
-
-export type DeleteDialogData = DialogData<'position'>;
